5c64f7697442ed9c3f9eee4db780897930b0ca68
June Cho <junecho.ucb@gmail.com>
2017-09-22 21:00:17 -0700

    Added EditorWithPlaceholder Control

---
+++ Controls/EditorWithPlaceholder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EditorPlaceholderEffectSample.Controls
+{
+    public class EditorWithPlaceholder
+    {
+        public EditorWithPlaceholder()
+        {
+        }
+    }
+}
