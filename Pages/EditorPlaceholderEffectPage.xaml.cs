a5efb98bde006cd33f7d72bf0714fe98538e5bde
June Cho <junecho.ucb@gmail.com>
2017-09-22 20:53:30 -0700

    created EditorPlaceholderEffectPage

---
+++ Pages/EditorPlaceholderEffectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EditorPlaceholderEffectSample.Pages
+{
+    public partial class EditorPlaceholderEffectPage : ContentPage
+    {
+        public EditorPlaceholderEffectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
